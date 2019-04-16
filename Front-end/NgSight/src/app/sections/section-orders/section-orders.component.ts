5281aced204cee1bea9bca2713b05c9bd8ce8e66
sandeeraDS <sandeerads@gmail.com>
2019-04-16 14:08:39 +0530

    added orders table

--- Front-end/NgSight/src/app/sections/section-orders/section-orders.component.ts
+++ Front-end/NgSight/src/app/sections/section-orders/section-orders.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Order } from "src/app/shared/models/order";
 
 @Component({
-  selector: 'app-section-orders',
-  templateUrl: './section-orders.component.html',
-  styleUrls: ['./section-orders.component.scss']
+  selector: "app-section-orders",
+  templateUrl: "./section-orders.component.html",
+  styleUrls: ["./section-orders.component.scss"]
 })
 export class SectionOrdersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: "Main St Bakery",
+        state: "CO",
+        email: "mainst@example.com"
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: "Completed"
+    },
+    {
+      id: 2,
+      customer: {
+        id: 1,
+        name: "Main St Bakery",
+        state: "CO",
+        email: "mainst@example.com"
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: "Completed"
+    },
+    {
+      id: 3,
+      customer: {
+        id: 1,
+        name: "Main St Bakery",
+        state: "CO",
+        email: "mainst@example.com"
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: "Completed"
+    },
+    {
+      id: 4,
+      customer: {
+        id: 1,
+        name: "Main St Bakery",
+        state: "CO",
+        email: "mainst@example.com"
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: "Completed"
+    },
+    {
+      id: 5,
+      customer: {
+        id: 1,
+        name: "Main St Bakery",
+        state: "CO",
+        email: "mainst@example.com"
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: "Completed"
+    }
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
