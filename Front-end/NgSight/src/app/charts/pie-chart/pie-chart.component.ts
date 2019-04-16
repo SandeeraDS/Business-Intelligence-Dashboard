afceb7e156871c6c74152592a4dd9eb4f937927a
sandeeraDS <sandeerads@gmail.com>
2019-04-16 09:53:15 +0530

    added pie chart

--- Front-end/NgSight/src/app/charts/pie-chart/pie-chart.component.ts
+++ Front-end/NgSight/src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.scss']
+  selector: "app-pie-chart",
+  templateUrl: "./pie-chart.component.html",
+  styleUrls: ["./pie-chart.component.scss"]
 })
 export class PieChartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = [
+    "XYZ Logistics",
+    "Main St Bakery",
+    "Acme Hosting"
+  ];
+  pieChartColors: any[] = [
+    {
+      backgroundColor: ["#26547c", "#ff6b6b", "#ffd166"],
+      borderColor: "#111"
+    }
+  ];
+  pieChartType = "doughnut";
+  ngOnInit() {}
 }
