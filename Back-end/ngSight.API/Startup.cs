0c07d55f5e9437ff3d14daf2c263eaa2d477b3d9
sandeeraDS <sandeerads@gmail.com>
2019-04-16 20:20:55 +0530

    connect with database

--- Back-end/ngSight.API/Startup.cs
+++ Back-end/ngSight.API/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ngSight.API.Models;
 
 namespace ngSight.API
 {
@@ -23,6 +25,8 @@ namespace ngSight.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NgSightDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
