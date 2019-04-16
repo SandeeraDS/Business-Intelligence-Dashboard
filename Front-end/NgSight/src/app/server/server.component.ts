80f18a302e6e2b2feaa9b50b5d9e8a6723dcdb57
sandeeraDS <sandeerads@gmail.com>
2019-04-16 15:55:32 +0530

    click event to toggle servers

--- Front-end/NgSight/src/app/server/server.component.ts
+++ Front-end/NgSight/src/app/server/server.component.ts
@@ -9,7 +9,28 @@ import { Server } from "../shared/models/server";
 export class ServerComponent implements OnInit {
   @Input() serverInput: Server;
 
+  color: string;
+  buttonText: string;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = "#66bb6a";
+      this.buttonText = "Shut Down";
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = "#ff6b6b";
+      this.buttonText = "Start";
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    this.serverInput.isOnline = !this.serverInput.isOnline;
+    this.setServerStatus(!onlineStatus);
+  }
 }
