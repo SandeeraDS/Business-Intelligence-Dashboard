bdddb3f3603d1ed6d8cb437312345a70dcd02f73
sandeeraDS <sandeerads@gmail.com>
2019-04-16 16:53:33 +0530

    created web api

---
+++ Back-end/ngSight.API/Models/NgSightDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ngSight.API.Models
+{
+    public class NgSightDbContext : DbContext
+    {
+        public NgSightDbContext(DbContextOptions<NgSightDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+    }
+}
